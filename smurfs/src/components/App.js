e074f5a8f223a847a346f9f0e4b6f984d48bad9d
chaycesol <chayce.solchaga@gmail.com>
2020-08-14 08:45:05 -0700

    fetch smurfs implemented

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,37 @@
 import React, { Component } from "react";
+import {connect} from 'react-redux';
+
+import { fetchData } from '../store/actions';
+
+
 import "./App.css";
 class App extends Component {
+  componentDidMount() {
+    this.props.fetchData()
+  }
+
   render() {
     return (
       <div className="App">
         <h1>SMURFS! W/Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        {this.props.smurfData &&
+          this.props.smurfData.map((smurf) => (
+            <div key={smurf.id}>
+              <p>{smurf.name}</p>
+              <p>{smurf.age}</p>
+              <p>{smurf.height}</p>
+            </div>
+          ))}
       </div>
     );
   }
 }
+const mapStateToProps = (state) => {
+  return {
+    isLoading: state.isLoading,
+    smurfData: state.smurfData,
+    error: state.error,
+  };
+};
 
-export default App;
+export default connect(mapStateToProps, { fetchData })(App);
